12890c532790b8210f9ad22fce5c306c434a1902
vmoura7 <v_moura@live.com>
2024-12-04 18:36:33 +0000

    feat: Implementação do layout de exportação do xml

--- src/Presenter/XmlExportPresenter.php
+++ src/Presenter/XmlExportPresenter.php
@@ -4,9 +4,19 @@ namespace Drupal\transparencia_export\Presenter;
 
 class XmlExportPresenter extends BaseExportPresenter
 {
-  protected function convertToFormat(string $html): string
+  protected function convertToFormat(array $data): string
   {
-    return '<?xml version="1.0" encoding="UTF-8"?><content>' . htmlspecialchars($html) . '</content>';
+    // Criar a estrutura XML com os dados fornecidos.
+    $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><conteudo></conteudo>');
+
+    // Adicionar os elementos ao XML.
+    $xml->addChild('titulo', htmlspecialchars($data['titulo']));
+    $xml->addChild('texto', htmlspecialchars($data['texto']));
+    $xml->addChild('url', htmlspecialchars($data['url']));
+    $xml->addChild('data', htmlspecialchars($data['data']));
+
+    // Retornar a string XML.
+    return $xml->asXML();
   }
 
   public function getHeaders(): array
